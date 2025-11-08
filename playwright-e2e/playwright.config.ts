7820abbc20021cfc74a01959c61b90a920749962
Manikandan <manikandanm441@gmail.com>
2025-11-08 18:55:54 +0530

    waits selcetors

--- playwright-e2e/playwright.config.ts
+++ playwright-e2e/playwright.config.ts
@@ -4,6 +4,7 @@ import {defineConfig} from '@playwright/test'
 export default defineConfig({
 
     testDir:'./e2e',
+    timeout: 120000,
     retries:0,
      reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
     use:{

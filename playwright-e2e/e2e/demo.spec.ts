861b2a33229a7e40baddf58e8b1144b03e321edf
Manikandan <manikandanm441@gmail.com>
2025-11-08 18:40:54 +0530

    report generation

--- playwright-e2e/e2e/demo.spec.ts
+++ playwright-e2e/e2e/demo.spec.ts
@@ -4,16 +4,10 @@ test('Connect to Chromium over CDP and test Angular app', async () => {
   // Connect to the Chromium instance via CDP
   const browser = await chromium.connectOverCDP('http://localhost:9093');
 
-  // Use the first context (usually the default one)
-  const context = browser.contexts()[0];
-  const page = await context.newPage();
-
-  // Navigate to the Angular app
-  await page.goto('http://localhost:4200');
-
-  var text = await page.locator("//app-home/p").textContent();
-  expect(text).toBe("home works!")
-
+  var context = browser.contexts()[0]
+  var page = await context.pages()[0] ?? await context.newPage()
+   var text = await page.locator("//app-home/p").textContent();
+   expect(text).toBe("home works!")
   // Clean up
   await context.close();
 });
--- playwright-e2e/playwright.config.ts
+++ playwright-e2e/playwright.config.ts
@@ -4,6 +4,7 @@ import {defineConfig} from '@playwright/test'
 export default defineConfig({
 
     testDir:'./e2e',
+     reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
     use:{
         headless:false
     }

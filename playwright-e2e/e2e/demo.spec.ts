16dfebae3bbd2c799bd3073aae1e99dc65c51361
Manikandan <manikandanm441@gmail.com>
2025-11-08 18:52:46 +0530

    change cdp code

--- playwright-e2e/e2e/demo.spec.ts
+++ playwright-e2e/e2e/demo.spec.ts
@@ -4,7 +4,7 @@ test('Connect to Chromium over CDP and test Angular app', async () => {
   // Connect to the Chromium instance via CDP
   const browser = await chromium.connectOverCDP('http://localhost:9093');
 
-  var context = browser.contexts()[0]
+  var context = browser.contexts()[0] ?? browser.newContext()
   var page = await context.pages()[0] ?? await context.newPage()
    var text = await page.locator("//app-home/p").textContent();
    expect(text).toBe("home works!")
--- playwright-e2e/playwright.config.ts
+++ playwright-e2e/playwright.config.ts
@@ -4,6 +4,7 @@ import {defineConfig} from '@playwright/test'
 export default defineConfig({
 
     testDir:'./e2e',
+    retries:0,
      reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
     use:{
         headless:false
